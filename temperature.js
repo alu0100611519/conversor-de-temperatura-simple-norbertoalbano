f3823528fb779def1c4073262d5fa32f91f2eec7
Norberto <alu0100611519@ull.edu.es>
2016-02-28 22:05:14 +0000

    acabada expresion regular

--- temperature.js
+++ temperature.js
@@ -2,13 +2,23 @@
 function calculate() {
   var result;
   var temp = original.value;
-  var regexp = /^([-+]?\d+(?:\.\d*)?)\s*(([fF]+[a]?[r]?)|[cC]+[e]?[l]?)$/;
+  var regexp = /^([-+]?\d+(?:\.\d*)?)\s*([e]?)\s*([-+]?\d+)?\s*(([fF]+[a]?[r]?)|[cC]+[e]?[l]?)$/;
 
   var m = temp.match(regexp);
 
   if (m) {
     var num = m[1];
+    if(m[2] == 'e')
+    {
+      var exp = m[3];
+      var type= m[6];
+      num = num * (Math.pow(10,exp));
+      console.log(exp);
+      console.log(m[3]);
+    }else{
     var type = m[2];
+    console.log(type);
+  }
     num = parseFloat(num);
     if (type == 'c' || type == 'C' || type == 'Ce' || type == 'Cel') {
       result = (num * 9/5)+32;
